27f8d7e5db862492f0f8ca2b7c7dc05dfb09c0e8
Christian <MbolanantenainaChristian@gmail.com>
2026-01-27 15:42:16 +0300

    ft_strncmp valide

---
+++ M0-LIBFT/ft_strncmp.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmbolana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 15:31:36 by jmbolana          #+#    #+#             */
+/*   Updated: 2026/01/27 15:41:46 by jmbolana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char *s1 = "MBOLA(ANTENAINA";
+	char *s2 = "MBOLAN";
+	int    d = ft_strncmp(s1, s2, 5);
+
+	printf("%d\n", d);
+	return (0);
+}*/
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n &&
+		s1[i] != '\0' &&
+		s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
